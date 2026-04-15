a13644aa32742dc0186368b46dde4ba5f87d95f7
Osmar Steffen <steffenosmar@hotmail.com>
2026-04-15 14:36:14 -0300

    inicio implementação

---
+++ ClubeDaLeitura.ConsoleApp/Dominio/Caixa.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ClubeDaLeitura.ConsoleApp.Dominio;
+
+public class Caixa
+{
+    
+}
---
+++ ClubeDaLeitura.ConsoleApp/Interface/Class.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ClubeDaLeitura.ConsoleApp.Interface;
+
+public class Class
+{
+    
+}
